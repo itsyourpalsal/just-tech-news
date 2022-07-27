693c3826a30a5db8c36dee2560ecda4c0bb483b8
Salvador Beltran <salbeltran1995@gmail.com>
2022-07-27 12:01:57 -0400

    small changes

--- config/connection.js
+++ config/connection.js
@@ -11,6 +11,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      password: '@Naruto1325',
       port: 3306
     });
   }
